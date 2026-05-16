4a0fdcd8ce3ca6a76009573753822560f1568d2b
Aaryan-Kapoor <aaryankapoor2006@gmail.com>
2026-05-15 22:22:21 -0400

    Frontend checkpoint. Sick

--- server/index.ts
+++ server/index.ts
@@ -62,6 +62,7 @@ app.use((req, res, next) => {
 });
 
 app.use(router);
+app.use("/demos", express.static(path.join(__dirname, "..", "surfaces")));
 app.use(express.static(path.join(__dirname, "..", "client")));
 
 app.listen(PORT, BIND, () => {
